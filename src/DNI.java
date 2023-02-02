8e5cb0b84e5f915e6bf36bd5ab7e62040eae16bb
maria-sancho <alusan7934@ieselcaminas.org>
2023-02-02 14:18:51 +0100

    DNI

--- src/DNI.java
+++ src/DNI.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class DNI {
+public class DNI {
+    private int number;
+    private char letter;
+
+    public DNI() {
+        this.number = 0;
+        this.letter = 'A';
+    }
+
+    public DNI(int number, char letter) {
+        this.number = number;
+        this.letter = letter;
+
+        if (!isValidLetter(letter)) {
+            this.number = -1;
+        }
+    }
+
+    public DNI(int number) {
+        this.number = number;
+        this.letter = calculateLetter(number);
+    }
+
+    public DNI(String s) {
+
+        this.number = Integer.parseInt(s.substring(0, s.length() - 1));
+        this.letter = s.charAt(s.length() - 1);
+
+        if (!isValidLetter(letter)) {
+            this.number = -1;
+        }
+    }
+
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public char getLetter() {
+        return this.letter;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+        this.letter = calculateLetter(number);
+    }
+
+    public String toString() {
+        return this.number + "" + this.letter;
+    }
+
+    public String toFormattedString() {
+        return String.format("%d.%03d.%03d-%c", this.number / 10000000, (this.number / 10000) % 1000, (this.number / 10) % 1000, this.letter);
+    }
+
+    public boolean correctDNI() {
+        return this.number >= 0 && isValidLetter(this.letter);
+    }
+
+    public static char letterForDni(int number) {
+        return calculateLetter(number);
+    }
+
+    public static String NifForDni(int number) {
+        char letter = calculateLetter(number);
+        return number + "" + letter;
+    }
+
+    private static char calculateLetter(int number) {
+        String letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int mod = number % 23;
+        char letter = letters.charAt(mod);
+        return letter;
+    }
+
+    private static boolean isValidLetter(char letter) {
+        String letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return letters.indexOf(letter) >= 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        DNI dni = new DNI("76515373P");
+        System.out.println(dni.getNumber());
+        System.out.println(dni.getLetter());
+        System.out.println(dni.toString());
+        System.out.println(dni.toFormattedString());
+        System.out.println(dni.correctDNI());
+        System.out.println(DNI.letterForDni(53793566));
+        System.out.println(DNI.NifForDni(53793566));
+    }
 }
